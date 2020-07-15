f8572624ca7ade29311786c5b0c19429cf4e6e09
haseeb <farooqhaseeb49@gmail.com>
2020-07-15 19:16:44 +0100

    add server side code

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
 const port = 5000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 app.get('/html/signup', (req, res) => {
-   res.render('html/signup'); 
+	res.render('html/signup');
 });
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.post('/create', async (req, res) => {
+	res.status(200).send(req.body);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
