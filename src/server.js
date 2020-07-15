d7e117e3ac4e2b63d0aaa9f7450d6d1e2c8f122c
gavintjhxx <gavintjhx@gmail.com>
2020-07-15 19:59:16 +0800

    Minor fixes and frontend skelebones setup

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
+const path = require("path");
+const express = require("express");
 const app = express();
 const port = 5000;
 
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
 
-app.get('/home', (req, res) => {
-   res.render('home'); 
+app.get('/html/home', (req, res) => {
+   res.render('html/home'); 
 });
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
