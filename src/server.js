52b9694db5b609279b9ccf34d156e1760618a5db
haseeb <farooqhaseeb49@gmail.com>
2020-07-16 14:24:47 +0100

    add security things on creating an account

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const argon2 = require('argon2');
 const app = express();
 const userModel = require('./database/models/user');
 const port = 5000;
+const database = require('./database/index');
+database.then(() => console.log('Connected to the database')).catch((e) => console.error(e));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
@@ -12,21 +14,38 @@ app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 app.get('/html/signup', (req, res) => {
-	res.render('html/signup');
+   res.render('html/signup');
 });
 
 app.post('/create', async (req, res) => {
-	const username = req.body.username;
-	const password = req.body.password;
-	console.log(req.body);
-	if (password.length <= 7) res.render('html/signup', { message: 'Passwords must be longer than 7 characters' });
-	if (username.length <= 7) res.render('html/signup', { message: 'Your username must be longer than 7 characters' });
-	const user = await userModel.create({
-		id: Date.now().toString(),
-		username,
-		password: await argon2.hash(password)
-	});
-	res.status(200).send(user);
+   const username = req.body.username;
+   const password = req.body.password;
+   const npassowrd = req.body.confirmPassword;
+   switch (true) {
+   	case password.length <= 7:
+   	    res.render('/html/signup', { message: 'That password is not strong enough.'});
+   	break;
+   	case username.toLowerCase() === password.toLowerCase():
+   	    res.render('/html/signup', { message: 'That password is not strong enough.'});
+   	break;
+   	case username.toLowerCase().split(' ').some((e) => password.toLowerCase().includes(e)):
+   	    res.render('/html/signup', { message: 'That password is not strong enough.'});
+   	break;
+   	case username.toLowerCase() === password.toLowerCase():
+   	    res.render('/html/signup', { message: 'That password is not strong enough.'});
+   	break;
+   }
+   const user = await userModel.create({
+      id: Date.now().toString(),
+      username,
+      password: await argon2.hash(password)
+   });
+   res.status(200).send(user);
 });
+
+app.post('/login', async (req, res) => {
+
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
